a47ae2a79bcb69b11dfc235eda45143b8430abb6
yelenchuk <t.elenchuk19@gmail>
2021-06-16 10:54:09 +0300

    timer

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,4 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
+  // Tab
   let tabs = document.querySelectorAll(".tabheader__item"),
     tabsContent = document.querySelectorAll(".tabcontent");
   tabParent = document.querySelector(".tabheader__items");
@@ -34,4 +35,46 @@ window.addEventListener("DOMContentLoaded", () => {
       });
     }
   });
+
+  // Timer
+  const deadline = "2020-05-11";
+
+  function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date()), // получим кол-во млсек, которые будут в нашем конечном времни
+      days = Мath.floor(t / (1000 * 60 * 60 * 24)), // считаем кол-во млсек и делим на кол-во млсек в одном дне
+      hours = Math.flor((t / (1000 * 60 * 60)) % 24), // используем % остаток часов, которые не переходят в сутки
+      minutes = Math.floor((t / (1000 / 60)) % 60),
+      seconds = Math.floor((t / 1000) % 60);
+
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minuts: minutes,
+      seconds: seconds,
+    };
+  }
+  function setClock(selector, andtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector("#days"),
+      hours = timer.querySelector("#hours"),
+      minutes = timer.querySelector("#minuts"),
+      seconds = timer.querySelector("#seconds"),
+      timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+      // обновление часов
+      const t = getTimeRemaining(endtime); // расчет того времени, кторое осталось на текущий момент
+
+      days.innerHTML = t.days;
+      hours.innerHTML = t.hours;
+      minutes.innerHTML = t.minutes;
+      seconds.innerHTML = t.seconds;
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval); // если вышло время, таймен не обновляем
+      }
+    }
+  }
+  setClock(".timer", deadline);
 });
