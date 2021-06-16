59aa5bf7ba3af88e1c931b35e2adbba7914e20fe
yelenchuk <t.elenchuk19@gmail>
2021-06-16 11:09:08 +0300

    timer

--- dist/js/index.js
+++ dist/js/index.js
@@ -37,34 +37,35 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 
   // Timer
-  const deadline = "2020-05-11";
+  const deadline = "2021-07-11";
 
   function getTimeRemaining(endtime) {
     const t = Date.parse(endtime) - Date.parse(new Date()), // получим кол-во млсек, которые будут в нашем конечном времни
-      days = Мath.floor(t / (1000 * 60 * 60 * 24)), // считаем кол-во млсек и делим на кол-во млсек в одном дне
-      hours = Math.flor((t / (1000 * 60 * 60)) % 24), // используем % остаток часов, которые не переходят в сутки
-      minutes = Math.floor((t / (1000 / 60)) % 60),
+      days = Math.floor(t / (1000 * 60 * 60 * 24)), // считаем кол-во млсек и делим на кол-во млсек в одном дне
+      hours = Math.floor((t / (1000 * 60 * 60)) % 24), // используем % остаток часов, которые не переходят в сутки
+      minutes = Math.floor((t / 1000 / 60) % 60),
       seconds = Math.floor((t / 1000) % 60);
 
     return {
       total: t,
       days: days,
       hours: hours,
-      minuts: minutes,
+      minutes: minutes,
       seconds: seconds,
     };
   }
-  function setClock(selector, andtime) {
+  function setClock(selector, endtime) {
     const timer = document.querySelector(selector),
       days = timer.querySelector("#days"),
       hours = timer.querySelector("#hours"),
-      minutes = timer.querySelector("#minuts"),
+      minutes = timer.querySelector("#minutes"),
       seconds = timer.querySelector("#seconds"),
       timeInterval = setInterval(updateClock, 1000);
 
     function updateClock() {
       // обновление часов
       const t = getTimeRemaining(endtime); // расчет того времени, кторое осталось на текущий момент
+      console.log(t);
 
       days.innerHTML = t.days;
       hours.innerHTML = t.hours;
