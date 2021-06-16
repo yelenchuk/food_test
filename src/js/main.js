5c6fe3a8fdca2e41b278eb6aef34161405bbe684
yelenchuk <t.elenchuk19@gmail>
2021-06-16 14:45:53 +0300

    NEW add tabs and timer fix

--- src/js/main.js
+++ src/js/main.js
@@ -24,7 +24,6 @@ window.addEventListener("DOMContentLoaded", () => {
   showTabContent();
 
   tabParent.addEventListener("click", function (event) {
-    console.log(event);
     const target = event.target;
     if (target && target.classList.contains("tabheader__item")) {
       tabs.forEach((item, i) => {
@@ -37,7 +36,7 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 
   // Timer
-  const deadline = "2021-07-11";
+  const deadline = "2021-06-18";
 
   function getTimeRemaining(endtime) {
     const t = Date.parse(endtime) - Date.parse(new Date()), // получим кол-во млсек, которые будут в нашем конечном времни
@@ -54,6 +53,16 @@ window.addEventListener("DOMContentLoaded", () => {
       seconds: seconds,
     };
   }
+
+  function getZero(num) {
+    //функция будет подставлять 0, года день или часы однозначное число
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
   function setClock(selector, endtime) {
     const timer = document.querySelector(selector),
       days = timer.querySelector("#days"),
@@ -61,16 +70,16 @@ window.addEventListener("DOMContentLoaded", () => {
       minutes = timer.querySelector("#minutes"),
       seconds = timer.querySelector("#seconds"),
       timeInterval = setInterval(updateClock, 1000);
+    updateClock(); //что-бы убрать мигание таймера при обновлении страницы
 
     function updateClock() {
       // обновление часов
       const t = getTimeRemaining(endtime); // расчет того времени, кторое осталось на текущий момент
-      console.log(t);
 
-      days.innerHTML = t.days;
-      hours.innerHTML = t.hours;
-      minutes.innerHTML = t.minutes;
-      seconds.innerHTML = t.seconds;
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
 
       if (t.total <= 0) {
         clearInterval(timeInterval); // если вышло время, таймен не обновляем
@@ -78,4 +87,20 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   }
   setClock(".timer", deadline);
+
+  // const testBtn = document.querySelector(".r_btn");
+  // testBtn.addEventListener("click", function () {
+  //   const testStr = document.querySelector(".test_time"),
+  //     timer = document.querySelector(".timer"),
+  //     days = timer.querySelector("#days"),
+  //     hours = timer.querySelector("#hours"),
+  //     minutes = timer.querySelector("#minutes"),
+  //     seconds = timer.querySelector("#seconds");
+
+  //   testStr.innerHTML = `Сейчас на таймере ${
+  //     days.textContent || days.innerText
+  //   } дня ${hours.textContent || hours.innerText} часов ${
+  //     minutes.textContent || minutes.innerText
+  //   } минут ${seconds.textContent || seconds.innerText} sec`;
+  // });
 });
