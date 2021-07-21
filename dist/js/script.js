8af14249ea72539416a7225161827437b65bd120
yelenchuk <t.elenchuk19@gmail>
2021-07-21 16:02:33 +0300

    lesson 63 slider add dots

--- dist/js/script.js
+++ dist/js/script.js
@@ -422,7 +422,8 @@ window.addEventListener("DOMContentLoaded", () => {
         current = document.querySelector("#current"),
         slidesWrapper = document.querySelector(".offer__slider-wrapper"),
         slidesField = document.querySelector(".offer__slider-inner"),
-        width = window.getComputedStyle(slidesWrapper).width;
+        width = window.getComputedStyle(slidesWrapper).width,
+        slider = document.querySelector(".offer__slider");
   let slideIndex = 1;
   let offSet = 0; // отсутуп впрово или влево, чтобы  понять как управлять трансформом
 
@@ -442,10 +443,71 @@ window.addEventListener("DOMContentLoaded", () => {
   slides.forEach(slide => {
     slide.style.width = width; // чтобы все слайды,независимо от их ширины, поместились в slidesfield
   });
+  slider.style.position = "relative"; // add Dots for Slider
+
+  const dotsWrapper = document.createElement("ol"),
+        dots = [];
+  dotsWrapper.classList.add("dot");
+  dotsWrapper.style.cssText = `
+   position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 15;
+    display: flex;
+    justify-content: center;
+    margin-right: 15%;
+    margin-left: 15%;
+    list-style: none;
+  `;
+  slider.append(dotsWrapper);
+
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement("li");
+    dot.setAttribute("data-slide-to", i + 1); // создавем атрибут и указываем, что атрибут
+
+    dot.style.cssText = `
+   box-sizing: content-box;
+    flex: 0 1 auto;
+    width: 30px;
+    height: 6px;
+    margin-right: 3px;
+    margin-left: 3px;
+    cursor: pointer;
+    background-color: #fff;
+    background-clip: padding-box;
+    border-top: 10px solid transparent;
+    border-bottom: 10px solid transparent;
+    opacity: .5;
+    transition: opacity .6s ease;
+  `;
+
+    if (i == 0) {
+      dot.style.opacity = 1; // делаем активную точки
+    }
+
+    dotsWrapper.append(dot);
+    dots.push(dot);
+  }
+
+  function activeDot() {
+    dots.forEach(dot => dot.style.opacity = "0.5");
+    dots[slideIndex - 1].style.opacity = 1;
+  }
+
+  function activeSlideNumber() {
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
+  }
+
   next.addEventListener("click", () => {
     //в условии преобразуем '500px' в 500px
     // == ширина 1 слайда + кол-во всех слайдов
     if (offSet == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+      console.log(width, width.length, offSet);
       offSet = 0;
     } else {
       offSet += +width.slice(0, width.length - 2); // когда мы нажимаем на стрелочку, добавляеться ширина еще одного слайда
@@ -459,11 +521,8 @@ window.addEventListener("DOMContentLoaded", () => {
       slideIndex++;
     }
 
-    if (slides.length < 10) {
-      current.textContent = `0${slideIndex}`;
-    } else {
-      current.textContent = slideIndex;
-    }
+    activeSlideNumber();
+    activeDot();
   });
   prev.addEventListener("click", () => {
     //в условии мы четко узнали, что сейчас четко 1й слайд
@@ -481,44 +540,18 @@ window.addEventListener("DOMContentLoaded", () => {
       slideIndex--;
     }
 
-    if (slides.length < 10) {
-      current.textContent = `0${slideIndex}`;
-    } else {
-      current.textContent = slideIndex;
-    }
-  }); // showSlides(slideIndex); // делаем инициалтиацию слайда
-  // if (slides.length < 10) {
-  //   total.textContent = `0${slides.length}`; // добавляем 0, если число слайдо меньше 10
-  // } else {
-  //   total.textContent = slides.length;
-  // }
-  // function showSlides(n) {
-  //   // если мы ушли в правую границу, то мы перемещаемся в самое начало слайдов
-  //   if (n > slides.length) {
-  //     slideIndex = 1;
-  //   }
-  //   // и наоборот
-  //   if (n < 1) {
-  //     slideIndex = slides.length;
-  //   }
-  //   slides.forEach((iteam) => iteam.classList.add("hide"));
-  //   slides[slideIndex - 1].classList.remove("hide");
-  //   slides[slideIndex - 1].classList.add("show");
-  //   if (slides.length < 10) {
-  //     current.textContent = `0${slideIndex}`;
-  //   } else {
-  //     current.textContent = slideIndex;
-  //   }
-  // }
-  // function plusSlides(n) {
-  //   showSlides((slideIndex += n));
-  // }
-  // prev.addEventListener("click", () => {
-  //   plusSlides(-1);
-  // });
-  // next.addEventListener("click", () => {
-  //   plusSlides(1);
-  // });
+    activeSlideNumber();
+    activeDot();
+  });
+  dots.forEach(dot => {
+    dot.addEventListener("click", e => {
+      const slideTo = e.target.getAttribute("data-slide-to");
+      slideIndex = slideTo;
+      offSet = +width.slice(0, width.length - 2) * (slideTo - 1);
+      activeSlideNumber();
+      activeDot();
+    });
+  });
 });
 
 /***/ })
--- src/js/main.js
+++ src/js/main.js
@@ -347,7 +347,9 @@ window.addEventListener("DOMContentLoaded", () => {
     current = document.querySelector("#current"),
     slidesWrapper = document.querySelector(".offer__slider-wrapper"),
     slidesField = document.querySelector(".offer__slider-inner"),
-    width = window.getComputedStyle(slidesWrapper).width;
+    width = window.getComputedStyle(slidesWrapper).width,
+    slider = document.querySelector(".offer__slider");
+
   let slideIndex = 1;
   let offSet = 0; // отсутуп впрово или влево, чтобы  понять как управлять трансформом
 
@@ -362,17 +364,75 @@ window.addEventListener("DOMContentLoaded", () => {
   slidesField.style.width = 100 * slides.length + "%";
   slidesField.style.display = "flex";
   slidesField.style.transitions = "0.5s all"; //  выравниваем слайды в одну линию по горизонтали
-
   slidesWrapper.style.overflow = "hidden";
 
   slides.forEach((slide) => {
     slide.style.width = width; // чтобы все слайды,независимо от их ширины, поместились в slidesfield
   });
 
+  slider.style.position = "relative";
+
+  // add Dots for Slider
+  const dotsWrapper = document.createElement("ol"),
+    dots = [];
+  dotsWrapper.classList.add("dot");
+  dotsWrapper.style.cssText = `
+   position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 15;
+    display: flex;
+    justify-content: center;
+    margin-right: 15%;
+    margin-left: 15%;
+    list-style: none;
+  `;
+  slider.append(dotsWrapper);
+
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement("li");
+    dot.setAttribute("data-slide-to", i + 1); // создавем атрибут и указываем, что атрибут
+    dot.style.cssText = `
+   box-sizing: content-box;
+    flex: 0 1 auto;
+    width: 30px;
+    height: 6px;
+    margin-right: 3px;
+    margin-left: 3px;
+    cursor: pointer;
+    background-color: #fff;
+    background-clip: padding-box;
+    border-top: 10px solid transparent;
+    border-bottom: 10px solid transparent;
+    opacity: .5;
+    transition: opacity .6s ease;
+  `;
+    if (i == 0) {
+      dot.style.opacity = 1; // делаем активную точки
+    }
+    dotsWrapper.append(dot);
+    dots.push(dot);
+  }
+
+  function activeDot() {
+    dots.forEach((dot) => (dot.style.opacity = "0.5"));
+    dots[slideIndex - 1].style.opacity = 1;
+  }
+
+  function activeSlideNumber() {
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
+  }
+
   next.addEventListener("click", () => {
     //в условии преобразуем '500px' в 500px
     // == ширина 1 слайда + кол-во всех слайдов
     if (offSet == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+      console.log(width, width.length, offSet);
       offSet = 0;
     } else {
       offSet += +width.slice(0, width.length - 2); // когда мы нажимаем на стрелочку, добавляеться ширина еще одного слайда
@@ -385,11 +445,8 @@ window.addEventListener("DOMContentLoaded", () => {
       slideIndex++;
     }
 
-    if (slides.length < 10) {
-      current.textContent = `0${slideIndex}`;
-    } else {
-      current.textContent = slideIndex;
-    }
+    activeSlideNumber();
+    activeDot();
   });
 
   prev.addEventListener("click", () => {
@@ -407,51 +464,19 @@ window.addEventListener("DOMContentLoaded", () => {
       slideIndex--;
     }
 
-    if (slides.length < 10) {
-      current.textContent = `0${slideIndex}`;
-    } else {
-      current.textContent = slideIndex;
-    }
+    activeSlideNumber();
+    activeDot();
   });
 
-  // showSlides(slideIndex); // делаем инициалтиацию слайда
+  dots.forEach((dot) => {
+    dot.addEventListener("click", (e) => {
+      const slideTo = e.target.getAttribute("data-slide-to");
 
-  // if (slides.length < 10) {
-  //   total.textContent = `0${slides.length}`; // добавляем 0, если число слайдо меньше 10
-  // } else {
-  //   total.textContent = slides.length;
-  // }
+      slideIndex = slideTo;
+      offSet = +width.slice(0, width.length - 2) * (slideTo - 1);
 
-  // function showSlides(n) {
-  //   // если мы ушли в правую границу, то мы перемещаемся в самое начало слайдов
-  //   if (n > slides.length) {
-  //     slideIndex = 1;
-  //   }
-  //   // и наоборот
-  //   if (n < 1) {
-  //     slideIndex = slides.length;
-  //   }
-
-  //   slides.forEach((iteam) => iteam.classList.add("hide"));
-  //   slides[slideIndex - 1].classList.remove("hide");
-  //   slides[slideIndex - 1].classList.add("show");
-
-  //   if (slides.length < 10) {
-  //     current.textContent = `0${slideIndex}`;
-  //   } else {
-  //     current.textContent = slideIndex;
-  //   }
-  // }
-
-  // function plusSlides(n) {
-  //   showSlides((slideIndex += n));
-  // }
-
-  // prev.addEventListener("click", () => {
-  //   plusSlides(-1);
-  // });
-
-  // next.addEventListener("click", () => {
-  //   plusSlides(1);
-  // });
+      activeSlideNumber();
+      activeDot();
+    });
+  });
 });
